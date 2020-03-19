cce7da01909db554cd11281b281daa045fbf8172
Umakant Sharma <Sharma_U@AtHoc-MacBook-Air.local>
2020-03-19 14:11:40 +0530

    update code

--- WeatherApp/app/src/test/java/com/weather/forecast/utils/ConstantsTest.kt
+++ WeatherApp/app/src/test/java/com/weather/forecast/utils/ConstantsTest.kt
@@ -5,6 +5,7 @@ import com.weather.forecast.utils.Constants.App_ID
 import com.weather.forecast.utils.Constants.Base_URL
 import com.weather.forecast.utils.Constants.CONNECTION_FAILURE_RESOLUTION_REQUEST
 import com.weather.forecast.utils.Constants.FREQUENCY
+import com.weather.forecast.utils.Constants.REPEAT_INTERVAL
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,7 +19,8 @@ class ConstantsTest {
     fun verifyConstants(){
         assertEquals(Base_URL, "http://api.openweathermap.org/data/2.5/")
         assertEquals(App_ID, "5ad7218f2e11df834b0eaf3a33a39d2a")
-        assertEquals(FREQUENCY, 2 * 60 * 60 * 1000)
+        assertEquals(FREQUENCY, 2)
+        assertEquals(REPEAT_INTERVAL, 2)
         assertEquals(CONNECTION_FAILURE_RESOLUTION_REQUEST, 9000)
         assertEquals(ACCESS_FINE_LOCATION_REQUEST_CODE, 200)
     }
